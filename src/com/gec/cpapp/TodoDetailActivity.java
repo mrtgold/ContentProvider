bb05dcd830e8dab25bdfe9c28c4fd0fed98a1c54
mrtgold <timothy.golding@gmail.com>
2013-01-24 05:21:27 -0600

    reformat

--- src/com/gec/cpapp/TodoDetailActivity.java
+++ src/com/gec/cpapp/TodoDetailActivity.java
@@ -18,137 +18,118 @@ import com.gec.cpapp.TodoTable;
  * TodoDetailActivity allows to enter a new todo item 
  * or to change an existing
  */
-public class TodoDetailActivity extends Activity
-{
-	private Spinner mCategory;
-	private EditText mTitleText;
-	private EditText mBodyText;
+public class TodoDetailActivity extends Activity {
+    private Spinner mCategory;
+    private EditText mTitleText;
+    private EditText mBodyText;
 
-	private Uri todoUri;
+    private Uri todoUri;
 
-	@Override
-	protected void onCreate(Bundle bundle)
-	{
-		super.onCreate(bundle);
-		setContentView(R.layout.todo_edit);
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.todo_edit);
 
-		mCategory = (Spinner) findViewById(R.id.category);
-		mTitleText = (EditText) findViewById(R.id.todo_edit_summary);
-		mBodyText = (EditText) findViewById(R.id.todo_edit_description);
-		Button confirmButton = (Button) findViewById(R.id.todo_edit_button);
+        mCategory = (Spinner) findViewById(R.id.category);
+        mTitleText = (EditText) findViewById(R.id.todo_edit_summary);
+        mBodyText = (EditText) findViewById(R.id.todo_edit_description);
+        Button confirmButton = (Button) findViewById(R.id.todo_edit_button);
 
-		Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
 
-		// Check from the saved Instance
-		todoUri = (bundle == null) ? null : (Uri) bundle
-			.getParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE);
+        // Check from the saved Instance
+        todoUri = (bundle == null) ? null : (Uri) bundle
+                .getParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE);
 
-		// Or passed from the other activity
-		if (extras != null)
-		{
-			todoUri = extras
-				.getParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE);
+        // Or passed from the other activity
+        if (extras != null) {
+            todoUri = extras
+                    .getParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE);
 
-			fillData(todoUri);
-		}
+            fillData(todoUri);
+        }
 
-		confirmButton.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View view)
-				{
-					if (TextUtils.isEmpty(mTitleText.getText().toString()))
-					{
-						makeToast();
-					}
-					else
-					{
-						setResult(RESULT_OK);
-						finish();
-					}
-				}
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(mTitleText.getText().toString())) {
+                    makeToast();
+                } else {
+                    setResult(RESULT_OK);
+                    finish();
+                }
+            }
 
-			});
-	}
+        });
+    }
 
-	private void fillData(Uri uri)
-	{
-		String[] projection = { TodoTable.COLUMN_SUMMARY,
-			TodoTable.COLUMN_DESCRIPTION, TodoTable.COLUMN_CATEGORY };
-		Cursor cursor = getContentResolver().query(uri, projection, null, null,
-												   null);
-		if (cursor != null)
-		{
-			cursor.moveToFirst();
-			String category = cursor.getString(cursor
-											   .getColumnIndexOrThrow(TodoTable.COLUMN_CATEGORY));
+    private void fillData(Uri uri) {
+        String[] projection = {TodoTable.COLUMN_SUMMARY,
+                TodoTable.COLUMN_DESCRIPTION, TodoTable.COLUMN_CATEGORY};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null,
+                null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            String category = cursor.getString(cursor
+                    .getColumnIndexOrThrow(TodoTable.COLUMN_CATEGORY));
 
-			for (int i = 0; i < mCategory.getCount(); i++)
-			{
+            for (int i = 0; i < mCategory.getCount(); i++) {
 
-				String s = (String) mCategory.getItemAtPosition(i);
-				if (s.equalsIgnoreCase(category))
-				{
-					mCategory.setSelection(i);
-				}
-			}
+                String s = (String) mCategory.getItemAtPosition(i);
+                if (s.equalsIgnoreCase(category)) {
+                    mCategory.setSelection(i);
+                }
+            }
 
-			mTitleText.setText(cursor.getString(cursor
-												.getColumnIndexOrThrow(TodoTable.COLUMN_SUMMARY)));
-			mBodyText.setText(cursor.getString(cursor
-											   .getColumnIndexOrThrow(TodoTable.COLUMN_DESCRIPTION)));
+            mTitleText.setText(cursor.getString(cursor
+                    .getColumnIndexOrThrow(TodoTable.COLUMN_SUMMARY)));
+            mBodyText.setText(cursor.getString(cursor
+                    .getColumnIndexOrThrow(TodoTable.COLUMN_DESCRIPTION)));
 
-			// Always close the cursor
-			cursor.close();
-		}
-	}
+            // Always close the cursor
+            cursor.close();
+        }
+    }
 
-	protected void onSaveInstanceState(Bundle outState)
-	{
-		super.onSaveInstanceState(outState);
-		saveState();
-		outState.putParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE, todoUri);
-	}
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putParcelable(MyTodoContentProvider.CONTENT_ITEM_TYPE, todoUri);
+    }
 
-	@Override
-	protected void onPause()
-	{
-		super.onPause();
-		saveState();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
 
-	private void saveState()
-	{
-		String category = (String) mCategory.getSelectedItem();
-		String summary = mTitleText.getText().toString();
-		String description = mBodyText.getText().toString();
+    private void saveState() {
+        String category = (String) mCategory.getSelectedItem();
+        String summary = mTitleText.getText().toString();
+        String description = mBodyText.getText().toString();
 
-		// Only save if either summary or description
-		// is available
+        // Only save if either summary or description
+        // is available
 
-		if (description.length() == 0 && summary.length() == 0)
-		{
-			return;
-		}
+        if (description.length() == 0 && summary.length() == 0) {
+            return;
+        }
 
-		ContentValues values = new ContentValues();
-		values.put(TodoTable.COLUMN_CATEGORY, category);
-		values.put(TodoTable.COLUMN_SUMMARY, summary);
-		values.put(TodoTable.COLUMN_DESCRIPTION, description);
+        ContentValues values = new ContentValues();
+        values.put(TodoTable.COLUMN_CATEGORY, category);
+        values.put(TodoTable.COLUMN_SUMMARY, summary);
+        values.put(TodoTable.COLUMN_DESCRIPTION, description);
 
-		if (todoUri == null)
-		{
-			// New todo
-			todoUri = getContentResolver().insert(MyTodoContentProvider.CONTENT_URI, values);
-		}
-		else
-		{
-			// Update todo
-			getContentResolver().update(todoUri, values, null, null);
-		}
-	}
+        if (todoUri == null) {
+            // New todo
+            todoUri = getContentResolver().insert(MyTodoContentProvider.CONTENT_URI, values);
+        } else {
+            // Update todo
+            getContentResolver().update(todoUri, values, null, null);
+        }
+    }
 
-	private void makeToast()
-	{
-		Toast.makeText(TodoDetailActivity.this, "Please maintain a summary",
-					   Toast.LENGTH_LONG).show();
-	}
+    private void makeToast() {
+        Toast.makeText(TodoDetailActivity.this, "Please maintain a summary",
+                Toast.LENGTH_LONG).show();
+    }
 }
--- src/com/gec/cpapp/TodosOverviewActivity.java
+++ src/com/gec/cpapp/TodosOverviewActivity.java
@@ -31,133 +31,120 @@ import com.gec.cpapp.TodoTable;
  */
 
 public class TodosOverviewActivity extends ListActivity implements
-LoaderManager.LoaderCallbacks<Cursor>
-{
-	private static final int ACTIVITY_CREATE = 0;
-	private static final int ACTIVITY_EDIT = 1;
-	private static final int DELETE_ID = Menu.FIRST + 1;
-	// private Cursor cursor;
-	private SimpleCursorAdapter adapter;
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int ACTIVITY_CREATE = 0;
+    private static final int ACTIVITY_EDIT = 1;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+    // private Cursor cursor;
+    private SimpleCursorAdapter adapter;
 
 
-	/** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.todo_list);
-		this.getListView().setDividerHeight(2);
-		fillData();
-		registerForContextMenu(getListView());
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_list);
+        this.getListView().setDividerHeight(2);
+        fillData();
+        registerForContextMenu(getListView());
+    }
 
-	// Create the menu based on the XML defintion
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.listmenu, menu);
-		return true;
-	}
+    // Create the menu based on the XML defintion
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.listmenu, menu);
+        return true;
+    }
 
-	// Reaction to the menu selection
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-			case R.id.insert:
-				createTodo();
-				return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    // Reaction to the menu selection
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert:
+                createTodo();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public boolean onContextItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-			case DELETE_ID:
-				AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-					.getMenuInfo();
-				Uri uri = Uri.parse(MyTodoContentProvider.CONTENT_URI + "/"
-									+ info.id);
-				getContentResolver().delete(uri, null, null);
-				fillData();
-				return true;
-		}
-		return super.onContextItemSelected(item);
-	}
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case DELETE_ID:
+                AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+                        .getMenuInfo();
+                Uri uri = Uri.parse(MyTodoContentProvider.CONTENT_URI + "/"
+                        + info.id);
+                getContentResolver().delete(uri, null, null);
+                fillData();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 
-	private void createTodo()
-	{
-		Intent i = new Intent(this, TodoDetailActivity.class);
-		//startActivity(i, ACTIVITY_CREATE); //orig, but doesn't compile
-		startActivity(i); //compiles, but is it right??
-	}
+    private void createTodo() {
+        Intent i = new Intent(this, TodoDetailActivity.class);
+        //startActivity(i, ACTIVITY_CREATE); //orig, but doesn't compile
+        startActivity(i); //compiles, but is it right??
+    }
 
-	// Opens the second activity if an entry is clicked
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id)
-	{
-		super.onListItemClick(l, v, position, id);
-		Intent i = new Intent(this, TodoDetailActivity.class);
-		Uri todoUri = Uri.parse(MyTodoContentProvider.CONTENT_URI + "/" + id);
-		i.putExtra(MyTodoContentProvider.CONTENT_ITEM_TYPE, todoUri);
+    // Opens the second activity if an entry is clicked
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent i = new Intent(this, TodoDetailActivity.class);
+        Uri todoUri = Uri.parse(MyTodoContentProvider.CONTENT_URI + "/" + id);
+        i.putExtra(MyTodoContentProvider.CONTENT_ITEM_TYPE, todoUri);
 
-		// Activity returns an result if called with startActivityForResult
-		//startActivity(i, ACTIVITY_EDIT); //orig, but doesn't compile
-		startActivity(i); //compiles, but is it right??
-	}
+        // Activity returns an result if called with startActivityForResult
+        //startActivity(i, ACTIVITY_EDIT); //orig, but doesn't compile
+        startActivity(i); //compiles, but is it right??
+    }
 
 
+    private void fillData() {
 
-	private void fillData()
-	{
+        // Fields from the database (projection)
+        // Must include the _id column for the adapter to work
+        String[] from = new String[]{TodoTable.COLUMN_SUMMARY};
+        // Fields on the UI to which we map
+        int[] to = new int[]{R.id.label};
 
-		// Fields from the database (projection)
-		// Must include the _id column for the adapter to work
-		String[] from = new String[] { TodoTable.COLUMN_SUMMARY };
-		// Fields on the UI to which we map
-		int[] to = new int[] { R.id.label };
+        getLoaderManager().initLoader(0, null, this);
+        adapter = new SimpleCursorAdapter(this, R.layout.todo_row, null, from,
+                to, 0);
 
-		getLoaderManager().initLoader(0, null, this);
-		adapter = new SimpleCursorAdapter(this, R.layout.todo_row, null, from,
-										  to, 0);
+        setListAdapter(adapter);
+    }
 
-		setListAdapter(adapter);
-	}
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+    }
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-									ContextMenuInfo menuInfo)
-	{
-		super.onCreateContextMenu(menu, v, menuInfo);
-		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
-	}
+    // Creates a new loader after the initLoader () call
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {TodoTable.COLUMN_ID, TodoTable.COLUMN_SUMMARY};
+        CursorLoader cursorLoader = new CursorLoader(this,
+                MyTodoContentProvider.CONTENT_URI, projection, null, null, null);
+        return cursorLoader;
+    }
 
-	// Creates a new loader after the initLoader () call
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args)
-	{
-		String[] projection = { TodoTable.COLUMN_ID, TodoTable.COLUMN_SUMMARY };
-		CursorLoader cursorLoader = new CursorLoader(this,
-													 MyTodoContentProvider.CONTENT_URI, projection, null, null, null);
-		return cursorLoader;
-	}
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data)
-	{
-		adapter.swapCursor(data);
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader)
-	{
-		// data is not available anymore, delete reference
-		adapter.swapCursor(null);
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // data is not available anymore, delete reference
+        adapter.swapCursor(null);
+    }
 }
